d969acca88db3571ee530963c0966a92a8ff0196
Ankita Kerpal <148011@s020rmc148-21.local>
2018-11-16 13:51:25 -0600

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Kerpal, Ankita R on 11/16/18.
+//  Copyright © 2018 Kerpal, Ankita R. All rights reserved.
+//
+
+import Foundation
