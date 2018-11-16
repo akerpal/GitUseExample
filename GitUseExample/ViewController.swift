d23bfb3ef869dfca7791d12fc40e7c190108b463
Ankita Kerpal <148011@s020rmc148-21.local>
2018-11-16 13:45:35 -0600

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Kerpal, Ankita R on 11/16/18.
+//  Copyright © 2018 Kerpal, Ankita R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
